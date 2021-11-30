21bfca556330783ebb196387428872ba359ca68a
DuCheAnh <ducheanh1308@gmail.com>
2021-12-01 01:11:08 +0700

    added: textblock bounces player and has animated sprites now

--- MultiplayerPuzzle/TestBlock.gd
+++ MultiplayerPuzzle/TestBlock.gd
@@ -3,7 +3,15 @@ extends Area2D
 var multiplier = 1
 
 func _on_TestBlock_body_entered(body):
-#	if body.is_in_group("Player"):
-#		if body.is_on_floor():
-#			body.jump(1)
-	pass
+	if body.is_in_group("Player"):
+		$Sprite.play("pressed")
+		$Sprite.scale = Vector2(1.25, 1)
+
+
+
+
+
+func _on_TestBlock_body_exited(body):
+	if body.is_in_group("Player"):
+		$Sprite.play("unpressed")
+		$Sprite.scale = $Sprite.scale.linear_interpolate(Vector2(1, 1),1)
