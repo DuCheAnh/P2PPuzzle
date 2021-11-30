3cd6fe683f5f5d37c34241ccf4871fbc3c35eeb8
DuCheAnh <ducheanh1308@gmail.com>
2021-11-30 23:44:44 +0700

    fixed: jump test block

--- MultiplayerPuzzle/Player.gd
+++ MultiplayerPuzzle/Player.gd
@@ -9,11 +9,13 @@ puppet var puppet_velocity = Vector2()
 puppet var puppet_sprite = ""
 puppet var puppet_flip = false
 puppet var puppet_scale = Vector2(1,1)
+puppet var puppet_hp = 10 setget puppet_hp_set
 
 onready var sprite = $AnimatedSprite
 onready var tween = $Tween
+onready var hit_timer = $HitTimer
 
-
+var hp = 10 setget set_hp
 var velocity = Vector2.ZERO
 var was_on_floor = false
 var gravity_disabled  = true
@@ -62,16 +64,30 @@ func _apply_animation() -> void:
 	#bounce when drop
 	if is_on_floor() and !was_on_floor:
 		sprite.scale = Vector2(1.1, 0.9)
+
 func _normalize_animation_scale(delta) -> void:
 	sprite.scale = sprite.scale.linear_interpolate(Vector2(1, 1), delta * 12)
+
 func _apply_animation_over_network() -> void:
 		sprite.play(puppet_sprite)
 		sprite.flip_h=puppet_flip
 		sprite.scale=puppet_scale
 
-func jump(multiplier) -> void:
+
+
+sync func jump(multiplier) -> void:
 	sprite.scale = Vector2(0.75, 1.25)
 	velocity.y = jump_speed * multiplier
+	modulate = Color(5,5,5,1)
+	hit_timer.start()
+func set_hp(new_value) -> void:
+	hp = new_value
+	if is_network_master():
+		rset("puppet_hp",hp)
+func puppet_hp_set(new_value) -> void:
+	puppet_hp = new_value
+	if not is_network_master():
+		hp = puppet_hp
 
 func player_gravity_set(value : float) -> void:
 	gravity_disabled = !value
@@ -82,6 +98,8 @@ func puppet_position_set(new_value) -> void:
 	tween.start()
 	pass
 
+
+
 func _on_Network_tick_rate_timeout():
 	if is_network_master():
 		rset_unreliable("puppet_position",global_position)
@@ -89,3 +107,17 @@ func _on_Network_tick_rate_timeout():
 		rset_unreliable("puppet_sprite",sprite.animation)
 		rset_unreliable("puppet_flip",sprite.flip_h)
 		rset_unreliable("puppet_scale",sprite.scale)
+
+
+func _on_HitBox_area_entered(area):
+	if get_tree().is_network_server():
+		if area.is_in_group("Player_jump_booster"):
+			rpc("jump",area.multiplier)
+
+
+
+
+func _on_HitTimer_timeout():
+	modulate = Color(1,1,1,1)
+
+
--- MultiplayerPuzzle/TestBlock.gd
+++ MultiplayerPuzzle/TestBlock.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-
+var multiplier = 1
 
 func _on_TestBlock_body_entered(body):
-	if body.is_in_group("Player"):
-		if body.is_on_floor():
-			body.jump(1)
-
+#	if body.is_in_group("Player"):
+#		if body.is_on_floor():
+#			body.jump(1)
+	pass
